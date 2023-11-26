75478b082c779dc7916cce3fe17e4ae257ef8799
Michael Girgis <michael.girgis@live.ca>
2023-11-25 23:45:47 -0500

    date and time added to landing page, time blocks have correct colour coding

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,56 @@
+var timeBlockNineEl = $('#hour-9');
+var timeBlockTenEl = $('#hour-10');
+var timeBlockElevenEl = $('#hour-11');
+var timeBlockTwelveEl = $('#hour-12');
+var timeBlockOneEl = $('#hour-1');
+var timeBlockTwoEl = $('#hour-2');
+var timeBlockThreeEl = $('#hour-3');
+var timeBlockFourEl = $('#hour-4');
+var timeBlockFiveEl = $('#hour-5');
+
+var DayHourBlocks = [9, 10, 11, 12, 1, 2, 3, 4, 5];
+var DayHourBlocksEl = [
+  timeBlockNineEl,
+  timeBlockTenEl,
+  timeBlockElevenEl,
+  timeBlockTwelveEl,
+  timeBlockOneEl,
+  timeBlockTwoEl,
+  timeBlockThreeEl,
+  timeBlockFourEl,
+  timeBlockFiveEl
+];
+
+function pastColor() {
+    before.addClass('past');
+}
+
+function currentColor() {
+  current.addClass('present');
+}
+
+function futureColor() {
+  later.addClass('future');
+}
+
+
+var timeNow = dayjs().format('H');
+console.log(timeNow);
+
+for (var i = 0; i < DayHourBlocks.length; i++) {
+  if (DayHourBlocks[i] < timeNow) {
+    var before = DayHourBlocksEl[i];
+   pastColor(before);
+  } else if (DayHourBlocks[i] = timeNow) {
+    var current = DayHourBlocksEl[i];
+    currentColor(current);
+    } else {
+      var later = DayHourBlocksEl[i];
+      futureColor(later);
+    }
+}
+
+
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
@@ -20,4 +73,10 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+  
+
 });
+
+var today = dayjs().format('dddd,MMMM D, YYYY h:mm A');
+$('#currentDay').text(today);
+
